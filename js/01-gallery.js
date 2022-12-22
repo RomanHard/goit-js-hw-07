2a2478ea1c585153a04ad3d01a1aa9b70a0c15fe
Roman <romeenice@gmail.com>
2022-12-22 23:41:47 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,35 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const palletContainet = document.querySelector(`.gallery`);
+const cardMarup = createItems(galleryItems);
+palletContainet.insertAdjacentHTML(`beforeend`, cardMarup);
 
-/* <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div> */
+palletContainet.addEventListener(`click`, onpalletContainetClick);
+
+function createItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, description, original }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${preview}">
+        <img
+          class="gallery__image"
+          src="${original}"
+           data-source="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+    `;
+    })
+    .join(``);
+}
+
+function onpalletContainetClick(evt) {
+  const imgContainer = evt.target.classList.contains(`gallery__item`);
+  if (!imgContainer) {
+    return;
+  }
+  console.log(evt.target);
+}
