b571388b04cbf294c15dfd8b8a3cb80c4b0a34dd
Roman <romeenice@gmail.com>
2022-12-23 21:30:50 +0200

    addGallery-items

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,27 +1,54 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+import { addGalleryItems } from "./addGallery-items.js";
 
 const refs = {
   imageContainer: document.querySelector(".gallery"),
   body: document.body,
 };
 
-function newgalleryItems(items) {
+const cardgalleryMarkup = makegalleryItems(newGalleryItems);
+
+refs.imageContainer.insertAdjacentHTML("beforeend", cardgalleryMarkup);
+
+function makegalleryItems(items) {
   return items
     .map(({ preview, description, original }) => {
-      return `<div class="gallery__item">
-      <a class="gallery__item" href="${original}">
-      <img loading="lazy" width="354" height="240"
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-      />
-    </a>
-  </div>`;
+      return `<li class="gallery__item"><a class="gallery__link" href="${original}">
+    <img loading="lazy" width="354" height="240" class="gallery__image" src="${preview}" alt="${description}" />
+  </a></li>`;
     })
     .join("");
 }
 
-const cardgalleryMarkup = newgalleryItems(galleryItems);
-refs.imageContainer.insertAdjacentHTML("beforeend", cardgalleryMarkup);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  scrollZoom: false,
+});
+
+lightbox.on("shown.simplelightbox", function () {
+  refs.body.classList.add("disable-scroll");
+});
+lightbox.on("closed.simplelightbox", function () {
+  refs.body.classList.remove("disable-scroll");
+});
+
+const lazyImages = refs.imageContainer.querySelectorAll(".gallery__image");
+
+lazyImages.forEach((image) =>
+  image.addEventListener("load", onImageLoaded, { once: true })
+);
+
+function onImageLoaded(event) {
+  event.target.classList.add("appear");
+}
+
+lazyImages.forEach((image) =>
+  image.addEventListener("mouseenter", onMouseEnter)
+);
+
+function onMouseEnter(event) {
+  event.target.style.transitionDelay = "100ms";
+  event.target.style.transitionDuration = "500ms";
+}
