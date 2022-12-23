56386b6a0c6caac586d4dc7f1bc176862e1aa47c
Roman <romeenice@gmail.com>
2022-12-23 21:18:07 +0200

    lightBox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  imageContainer: document.querySelector(".gallery"),
+  body: document.body,
+};
+
+function newgalleryItems(items) {
+  return items
+    .map(({ preview, description, original }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__item" href="${original}">
+      <img loading="lazy" width="354" height="240"
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join("");
+}
+
+const cardgalleryMarkup = newgalleryItems(galleryItems);
+refs.imageContainer.insertAdjacentHTML("beforeend", cardgalleryMarkup);
