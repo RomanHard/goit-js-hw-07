c821433f67c1224b17d63c044ef82158ded77616
Roman <romeenice@gmail.com>
2022-12-23 20:47:41 +0200

    summary global update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,35 +1,82 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const refs = {
+  imageContainer: document.querySelector(".gallery"),
+  body: document.body,
+};
 
-const palletContainet = document.querySelector(`div.gallery`);
-const cardMarup = createItems(galleryItems);
-palletContainet.insertAdjacentHTML(`beforeend`, cardMarup);
-
-palletContainet.addEventListener(`click`, onpalletContainetClick);
-
-function createItems(galleryItems) {
-  return galleryItems
+function newgalleryItems(items) {
+  return items
     .map(({ preview, description, original }) => {
-      return `
-    <div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img loading="lazy" width="354" height="240"
-          class="gallery__image"
-          src="${preview}"
-           data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </div>
-    `;
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img loading="lazy" width="354" height="240"
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
     })
-    .join(``);
+    .join("");
 }
 
-function onpalletContainetClick(evt) {
-  const imgContainer = evt.target.classList.contains(`gallery__item`);
-  if (!imgContainer) {
+const cardgalleryMarkup = newgalleryItems(galleryItems);
+refs.imageContainer.insertAdjacentHTML("beforeend", cardgalleryMarkup);
+
+const createModalWindow = (imageAdress) => {
+  window.instance = basicLightbox.create(
+    `
+    <img src="${imageAdress}">
+`,
+    {
+      onShow: () =>
+        window.addEventListener("keydown", closeModalWindowByEscPressing),
+      onClose: () => {
+        window.removeEventListener("keydown", closeModalWindowByEscPressing);
+        refs.body.classList.remove("disable-scroll");
+      },
+    }
+  );
+  return instance;
+};
+
+refs.imageContainer.addEventListener("click", onClickOpenModal);
+
+function onClickOpenModal(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
     return;
   }
-  console.log(evt.target);
+  const originalImageRef = event.target.dataset.source;
+  createModalWindow(originalImageRef).show();
+  refs.body.classList.add("disable-scroll");
+}
+
+function closeModalWindowByEscPressing(event) {
+  const ESC_KEY_CODE = "Escape";
+  if (event.code === ESC_KEY_CODE && instance.visible()) {
+    instance.close();
+    refs.body.classList.remove("disable-scroll");
+  }
+}
+
+const lazyImages = refs.imageContainer.querySelectorAll(".gallery__image");
+
+lazyImages.forEach((image) =>
+  image.addEventListener("load", onImageLoaded, { once: true })
+);
+
+function onImageLoaded(event) {
+  event.target.classList.add("appear");
+}
+
+lazyImages.forEach((image) =>
+  image.addEventListener("mouseenter", onMouseEnter)
+);
+
+function onMouseEnter(event) {
+  event.target.style.transitionDelay = "100ms";
+  event.target.style.transitionDuration = "500ms";
 }
